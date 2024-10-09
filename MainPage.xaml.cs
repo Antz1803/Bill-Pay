a8479351216b24a4495014360aeeb7c716668ce5
Antz1803 <act.jrantipaso@gmail.com>
2024-10-09 11:32:00 +0800

    hahas

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,5 +1,7 @@
 ﻿namespace MancillaBillPay
 {
+<<<<<<< HEAD
+<<<<<<< HEAD
 	public partial class MainPage : ContentPage
 	{
 
@@ -159,3 +161,65 @@
 	}
   }
 
+=======
+=======
+>>>>>>> f8cf492ef797c426361b307114774a3d035eccfd
+    public partial class MainPage : ContentPage
+    {
+     
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCustomize(object sender, EventArgs e)
+        {
+            CustomizeTip.IsVisible = true;
+            AutomatedTip.IsVisible = false;
+        }
+
+        private void OnAutomated(object sender, EventArgs e)
+        {
+            AutomatedTip.IsVisible = true;
+            CustomizeTip.IsVisible = false;
+        }
+
+        private int personCount = 0;
+
+        private void OnIncrementClicked(object sender, EventArgs e)
+        {
+            personCount++;
+            PersonCountLabelOne.Text = personCount.ToString();
+        }
+
+        private void OnDecrementClicked(object sender, EventArgs e)
+        {
+            if (personCount > 0)
+            {
+                personCount--;
+                PersonCountLabelOne.Text = personCount.ToString();
+            }
+        }
+        private void OnIncrementClickedOne(object sender, EventArgs e)
+        {
+            personCount++;
+            PersonCountLabelTwo.Text = personCount.ToString();
+        }
+
+        private void OnDecrementClickedOne(object sender, EventArgs e)
+        {
+            if (personCount > 0)
+            {
+                personCount--;
+                PersonCountLabelTwo.Text = personCount.ToString();
+            }
+        }
+
+    }
+
+}
+<<<<<<< HEAD
+>>>>>>> f8cf492ef797c426361b307114774a3d035eccfd
+=======
+>>>>>>> f8cf492ef797c426361b307114774a3d035eccfd
